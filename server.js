056231bf54d776f7589108ee9fae3c2787759446
mangeshchauhan <mangesh.225@gmail.com>
2026-01-04 10:24:08 +0530

    updating the server.js file

--- server.js
+++ server.js
@@ -11,11 +11,14 @@ const __dirname = dirname(__filename);
 const app = express();
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '',
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'student_apps',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 app.use(cors());
